e7ebb9df9ffec043595cc2714fcdc216c1a57bba
brunoalbim <>
2023-01-01 20:14:44 +0000

    Initial commit

---
+++ functions.php
@@ -0,0 +1,43 @@
+<?php
+
+function encrypt($plaintext, $key, $cipher = "aes-256-gcm") {
+    if (!in_array($cipher, openssl_get_cipher_methods())) {
+        return false;
+    }
+    $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length($cipher));
+    $tag = null;
+    $ciphertext = openssl_encrypt(
+        gzcompress($plaintext),
+        $cipher,
+        $key,
+        $options=0,
+        $iv,
+        $tag,
+    );
+    return json_encode(
+        array(
+            "ciphertext" => base64_encode($ciphertext),
+            "cipher" => $cipher,
+            "iv" => base64_encode($iv),
+            "tag" => base64_encode($tag),
+        )
+    );
+}
+
+function decrypt($cipherjson, $key) {
+    try {
+        $json = json_decode($cipherjson, true, 2,  JSON_THROW_ON_ERROR);
+    } catch (Exception $e) {
+        return false;
+    }
+    return gzuncompress(
+        openssl_decrypt(
+            base64_decode($json['ciphertext']),
+            $json['cipher'],
+            $key,
+            $options=0,
+            base64_decode($json['iv']),
+            base64_decode($json['tag'])
+        )
+    );
+}
---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+require_once('functions.php');
+?>
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.15.15/dist/css/uikit.min.css" />
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.15.15/dist/js/uikit.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.15.15/dist/js/uikit-icons.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
+
+
+
+<div class="uk-container uk-container-xsmall uk-margin-top">
+  <p> <strong>Criptografar e Descriptografar mensagens</strong> </p>
+  <form action="" method="post">
+    <select class="uk-select" name="tipo">
+      <option>Criptografar</option>
+      <option>Descriptografar</option>
+    </select><br><br>
+    <textarea rows="8" required class="uk-textarea" name="mensagem" placeholder="mensagem"></textarea><br><br>
+    <input required class="uk-input" type="text" name="senha" id="senha" placeholder="senha">
+    <a href="#" onclick="getPassword()">Gerar senha forte</a><br><br>
+    <button class="uk-button uk-button-primary" type="submit">Gerar</button>
+    <a href="" class="uk-button">Resetar</a>
+  </form>
+  
+  <br>
+  <p class="uk-alert" style="overflow-wrap: break-word;"><?php echo $_POST['tipo'] === "Criptografar" ? encrypt($_POST['mensagem'], $_POST['senha']) :
+    decrypt($_POST['mensagem'], $_POST['senha']); ?></p>
+</div>
+
+<script>
+function getPassword() {
+  var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJLMNOPQRSTUVWXYZ!@#$&";
+  var passwordLength = 20;
+  var password = "";
+
+  for (var i = 0; i < passwordLength; i++) {
+    var randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber + 1);
+  }
+  document.getElementById('senha').value = password
+}
+</script>
